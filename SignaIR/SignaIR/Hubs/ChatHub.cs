0c29bb23037349994159edb1f10880c46c0912dc
rahimliparvin <parvinvr@code.edu.az>
2023-12-25 15:41:27 +0400

    SignalR ready

---
+++ SignaIR/SignaIR/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignaIR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string username ,string message)
+        {
+            await Clients.All.SendAsync("RecieveMessage",username,message);
+        }
+    }
+}
